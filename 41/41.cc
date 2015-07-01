37c3a96a705ba6569e1a853e3e4f0181021a2421
tianmn2109 <tianmn2109ccst@163.com>
2015-07-01 10:21:29 +0800

    Add problem 41: 最大子数组

---
+++ 41/41.cc
@@ -0,0 +1,25 @@
+class Solution {
+public:    
+    /**
+     * @param nums: A list of integers
+     * @return: A integer indicate the sum of max subarray
+     */
+    int maxSubArray(vector<int> nums) {
+        // write your code here
+        if (nums.size() == 0)
+            return 0;
+        if (nums.size() == 1)
+            return nums[0];
+        int b = nums[0];
+        int maxSum = nums[0];
+        for (int i = 0; i < nums.size(); i ++) {
+            if (b > 0)
+                b = b + nums[i];
+            else 
+                b = nums[i];
+            maxSum = max(maxSum, b);
+        }
+        return maxSum;
+    }
+};
+
