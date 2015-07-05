8ea049ce74d84efd852ba361329b392151646861
tianmn2109 <tianmn2109ccst@163.com>
2015-07-05 15:29:40 +0800

    Add problem 172: 删除元素

---
+++ 172/172.cc
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    /** 
+     *@param A: A list of integers
+     *@param elem: An integer
+     *@return: The new length after remove
+     */
+    int removeElement(vector<int> &A, int elem) {
+        // write your code here
+        int k = 0;
+        for (int i = 0; i < A.size(); i ++)
+            if (A[i] != elem)
+                A[k ++] = A[i];
+        return k;
+    }
+};
+
