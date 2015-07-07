a00bb64772012bf38e41659d5b157242ef776db3
tianmn2109 <tianmn2109ccst@163.com>
2015-07-07 15:03:39 +0800

    Add problem 63: 搜索旋转排序数组 II

---
+++ 63/63.cc
@@ -0,0 +1,34 @@
+class Solution {
+    /** 
+     * param A : an integer ratated sorted array and duplicates are allowed
+     * param target :  an integer to be search
+     * return : a boolean 
+     */
+public:
+    bool search(vector<int> &A, int target) {
+        // write your code here
+        int left = 0;
+        int right = A.size() - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (A[mid] == target)
+                return mid;
+            if (A[mid] > A[right]) {
+                if (A[left] <= target && target < A[mid])
+                    right = mid - 1;
+                else
+                    left = mid + 1;
+            }
+            else if (A[mid] < A[right]) {
+                if (A[mid] < target && target <= A[right])
+                    left = mid + 1;
+                else 
+                    right = mid - 1;
+            }
+            else 
+                right --;
+        }
+        return false;
+    }
+};
+
